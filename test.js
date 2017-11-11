6508bd96ad3de16cf1ed4c172ab33eb4ea670378
Sednaoui <marcsednaoui@hotmail.com>
2017-11-11 17:43:02 -0500

    function added2

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const wrapper = require('./wrapper');
 
-function callWrapper(){
-  wrapper.movingAverage("FB",300,(average) => {
-    console.log(average);
+
+wrapper.movingAverage("FB",300,(average1) => {
+  wrapper.movingAverage("FB",600, (average2) => {
+    if(average1>average2){
+    //do something
+    }
   })
-}
-callWrapper();
+})
