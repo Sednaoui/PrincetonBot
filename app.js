21685956783219b8fe18521d4786eca2dfbdc8ee
Sednaoui <marcsednaoui@hotmail.com>
2017-11-11 13:18:57 -0500

    test API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
+A simple echo bot for the Microsoft Bot Framework.
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
@@ -8,22 +8,22 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
     stateEndpoint: process.env.BotStateEndpoint,
-    openIdMetadata: process.env.BotOpenIdMetadata 
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
-// Listen for messages from users 
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* Bot Storage: This is a great spot to register the private state storage for your bot.
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
@@ -34,10 +34,15 @@ var bot = new builder.UniversalBot(connector);
 bot.dialog('/', [
     function (session) {
         builder.Prompts.text(session, "Hello... What's your name?");
+        var request = require('request');
+        request('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=82ISQ1AMN633ECH8', function (error, response, body) {
+          //console.log('error:', error); // Print the error if one occurred
+          //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          //console.log('body:', body); // Print the HTML for the Google homepage.
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+        builder.Prompts.number(session, "Hi" + response && results.statusCode);
     },
     function (session, results) {
         session.userData.coding = results.response;
@@ -45,8 +50,8 @@ bot.dialog('/', [
     },
     function (session, results) {
         session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
+        session.send("Got it... " + session.userData.name +
+                    " you've been programming for " + session.userData.coding +
                     " years and use " + session.userData.language + ".");
     }
 ]);
