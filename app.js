a775dbf31baf8d29ea645e5b55ed3ea7a7bf39fc
richard-salaschavez <r.i.salas.jr@gmail.com>
2017-11-11 19:18:23 -0500

    added new questions

--- app.js
+++ app.js
@@ -46,19 +46,23 @@ bot.dialog('/', [
     function (session) {
         builder.Prompts.text(session, "Hello... What's your name?");
     },
+    
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+        session.userData.longterm = results.response.entity;
+        session.send("Got it... ");
+        builder.Prompts.number(session, "How many days do you consider to be short-term?");
     },
+
     function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+        session.userData.shortterm = results.response.entity;
+        session.send("Got it... ");
+        builder.Prompts.number(session, "How much % of a cushion do you want before we make a purchase?");
     },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name +
-                    " you've been programming for " + session.userData.coding +
-                    " years and use " + session.userData.language + ".");
+
+    function (session, result) {
+        session.userData.cushion = results.response.entity/100.0;
+        session.send("Alright! just one more question and I'll take care of the rest!");
+        builder.Prompts.number(session, "How many shares would you like?");
     }
 ]);
 
